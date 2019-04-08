edf3562348a5c512cfc4ad638138c7c2b512ca0f
jjgorton <jjgorton@gmail.com>
2019-04-08 18:50:15 -0400

    MVP and Stretch for Counter Project Complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,42 +1,53 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
 class Counter extends Component {
-    incrementIfOdd = () => {
-        // Stretch Problem: Implement an increment function that
-        // only increments if the counter value is odd
-    };
+	incrementIfOdd = () => {
+		// Stretch Problem: Implement an increment function that
+		// only increments if the counter value is odd
+		if (this.props.count % 2 !== 0) {
+			this.props.increment(this.props.count);
+		}
+	};
 
-    incrementAsync = () => {
-        // Stretch Problem: Implement an increment function that
-        // increments after waiting for one second
-    };
+	incrementAsync = () => {
+		// Stretch Problem: Implement an increment function that
+		// increments after waiting for one second
+		setTimeout(() => this.props.increment(this.props.count), 1000);
+	};
 
-    render() {
-        // Fill in the two button onClick methods
-        // Upon clicking these buttons, the count
-        // should decrement or increment accordingly
-        return (
-            <p>
-                Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
-                    +
-                </button>
-                <button onClick={() => {/* Fill me in */ }}>
-                    -
-                </button>
-                 {/* Uncomment these button tags if you got
+	render() {
+		// Fill in the two button onClick methods
+		// Upon clicking these buttons, the count
+		// should decrement or increment accordingly
+		return (
+			<p>
+				Clicked: {this.props.count} times
+				<button
+					onClick={() => {
+						/* Fill me in */
+						this.props.increment(this.props.count);
+					}}
+				>
+					+
+				</button>
+				<button
+					onClick={() => {
+						/* Fill me in */
+						console.log('minus');
+						this.props.decrement();
+					}}
+				>
+					-
+				</button>
+				{/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
-                    Increment if odd
-                </button>
-                <button onClick={this.incrementAsync}>
-                    Increment async
-                </button>  */}
-            </p>
-        );
-    }
+				<button onClick={this.incrementIfOdd}>Increment if odd</button>
+				<button onClick={this.incrementAsync}>Increment async</button>
+			</p>
+		);
+	}
 }
 
 // The mapStateToProps function specifies which portion of the
@@ -46,9 +57,9 @@ class Counter extends Component {
 // redux application, though, it would receive only the relevant
 // parts it needs from the state object.
 const mapStateToProps = (state) => {
-    return {
-        count: state.count
-    };
+	return {
+		count : state.count
+	};
 };
 
 // The connect function is called in order to make this component aware
